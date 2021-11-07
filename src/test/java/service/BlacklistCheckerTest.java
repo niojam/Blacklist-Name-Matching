377d442ebac041a508c851687cb3073fa12d5c42
nikita.ojamae <nikita.ojamae@wemakesoftware.eu>
2021-11-07 15:23:59 +0200

    Clean code

--- src/test/java/service/BlacklistCheckerTest.java
+++ src/test/java/service/BlacklistCheckerTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BlacklistCheckerTest {
 
+    public static final String TEST_BLACKLIST_FILE_NAME = "names.txt";
+    public static final String TEST_NOISE_WORDS_FILE_NAME = "noise_words.txt";
     private static BlacklistChecker blacklistChecker;
 
     @BeforeAll
@@ -16,46 +18,46 @@ class BlacklistCheckerTest {
 
     @Test
     void isNameInBlackList() {
-        assertTrue(blacklistChecker.isNameInBlackList("Osama Bin Laden", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Joe Luis Webb", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("George Bush", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Neale Armstrong", "names.txt", "noise_words.txt"));
+        assertTrue(blacklistChecker.isNameInBlackList("Osama Bin Laden", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Joe Luis Webb", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("George Bush", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Neale Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
     }
 
     @Test
     void isNameInBlackListNotInBlackList() {
-        assertFalse(blacklistChecker.isNameInBlackList("Nikita Ojamae", "names.txt", "noise_words.txt"));
-        assertFalse(blacklistChecker.isNameInBlackList("John Bush", "names.txt", "noise_words.txt"));
-        assertFalse(blacklistChecker.isNameInBlackList("Awad bin Laden", "names.txt", "noise_words.txt"));
-        assertFalse(blacklistChecker.isNameInBlackList("Nethan Armstrong", "names.txt", "noise_words.txt"));
-        assertFalse(blacklistChecker.isNameInBlackList("Neale Armsterdam", "names.txt", "noise_words.txt"));
+        assertFalse(blacklistChecker.isNameInBlackList("Nikita Ojamae", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertFalse(blacklistChecker.isNameInBlackList("John Bush", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertFalse(blacklistChecker.isNameInBlackList("Awad bin Laden", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertFalse(blacklistChecker.isNameInBlackList("Nethan Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertFalse(blacklistChecker.isNameInBlackList("Neale Armsterdam", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
     }
 
     @Test
     void isNameInBlackListNoiseWords() {
-        assertTrue(blacklistChecker.isNameInBlackList("to the osama bin laden", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("osama and bin laden", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("I am George and Bush", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("There is George Bush", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Neale and Armstrong", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("in the Neale and Armstrong", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("at Neale and on Armstrong", "names.txt", "noise_words.txt"));
+        assertTrue(blacklistChecker.isNameInBlackList("to the osama bin laden", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("osama and bin laden", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("I am George and Bush", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("There is George Bush", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Neale and Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("in the Neale and Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("at Neale and on Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
     }
 
     @Test
     void isNameInBlackListTypos() {
-        assertTrue(blacklistChecker.isNameInBlackList("Osam Bin Laiden", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Jo Luis Webb", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Georg Buch", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Niile Armstrong", "names.txt", "noise_words.txt"));
+        assertTrue(blacklistChecker.isNameInBlackList("Osam Bin Laiden", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Jo Luis Webb", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Georg Buch", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Niile Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
     }
 
     @Test
     void isNameInBlackListTyposAndNoise() {
-        assertTrue(blacklistChecker.isNameInBlackList("Osam and Bin the Laden", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("to a Jo Luis Webb", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("I am Georg  Buch", "names.txt", "noise_words.txt"));
-        assertTrue(blacklistChecker.isNameInBlackList("Mr Niile Armstrong", "names.txt", "noise_words.txt"));
+        assertTrue(blacklistChecker.isNameInBlackList("Osam and Bin the Laden", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("to a Jo Luis Webb", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("I am Georg  Buch", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
+        assertTrue(blacklistChecker.isNameInBlackList("Mr Niile Armstrong", TEST_BLACKLIST_FILE_NAME, TEST_NOISE_WORDS_FILE_NAME));
     }
 
 }
